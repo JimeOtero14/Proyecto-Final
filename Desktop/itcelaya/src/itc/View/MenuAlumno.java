48e15c87e573a39bbd31793abd005776079b9319
MORAN GARCIA DIEGO <22031317@itcelaya.edu.mx>
2025-06-02 20:45:58 -0500

    listo

--- Desktop/itcelaya/src/itc/View/MenuAlumno.java
+++ Desktop/itcelaya/src/itc/View/MenuAlumno.java
@@ -14,7 +14,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+//hola
 import java.io.ByteArrayInputStream;
 
 public class MenuAlumno {
